3119be64a948fc356be05f84fd03b6c8eea65df3
Greg Griffin <griffin.ge@gmail.com>
2017-09-28 09:46:38 -0700

    New directory structure: by week

---
+++ week3/howMany.py
@@ -0,0 +1,19 @@
+animals = { 'a': ['aardvark'], 'b': ['baboon'], 'c': ['coati']}
+animals['d'] = ['donkey']
+animals['d'].append('dog')
+animals['d'].append('dingo')
+
+
+def how_many(aDict):
+    '''
+    aDict: A dictionary, where all the values are lists.
+
+    returns: int, how many values are in the dictionary.
+    '''
+    total = 0
+    for key in aDict:
+        total += len(aDict[key])
+                    
+    return total
+
+print(how_many(animals))
