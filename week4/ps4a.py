0f68b5d72abac6ca019439abf8b912217b9ca7c7
Greg Griffin <griffin.ge@gmail.com>
2017-10-19 14:42:00 -0700

    PlayGame working

--- week4/ps4a.py
+++ week4/ps4a.py
@@ -275,13 +275,30 @@ def playGame(wordList):
       * If the user inputs 'n', let the user play a new (random) hand.
       * If the user inputs 'r', let the user play the last hand again.
       * If the user inputs 'e', exit the game.
-      * If the user inputs anything else, tell them their input was invalid.
+      * If the user inputs anything else, tell them their input was invalid.N
 
     2) When done playing the hand, repeat from step 1
     """
-    # TO DO ... <-- Remove this comment when you code this function
-    print("playGame not yet implemented.") # <-- Remove this line when you code the function
 
+    while True:
+
+        userInput = input("Enter n to deal a new hand, r to replay the last hand, or e to the the game: ")
+
+        if userInput == 'n':
+            hand = dealHand(HAND_SIZE)
+            playHand(hand, wordList, HAND_SIZE)
+
+        elif userInput == 'r':
+            try:
+                playHand(hand, wordList, HAND_SIZE)
+            except NameError:
+                print('You have not played a hand yet. Please play a new hand first!')
+
+        elif userInput == 'e':
+            break
+
+        else:
+            print('Invalid Command.')
 
 
 
