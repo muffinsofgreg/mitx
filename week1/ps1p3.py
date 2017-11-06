6dd9c195956b22f1e212f4d96013ccf596ffeedc
Greg Griffin <griffin.ge@gmail.com>
2017-11-06 06:57:16 -0800

    99% complete, need case for full string

--- week1/ps1p3.py
+++ week1/ps1p3.py
@@ -1,15 +1,30 @@
 #Assume s is s string of lowercase characters
-s = 'iykjhasdfnasdopunasdbaiwuabdcourys'
+s = 'abcdeflaiydgakjsndfrstuvwxy'
 
-longest = []
 
-for i in range(len(s)):
-    for x in range(len(s)):
-        if s[i] < s[x]:
-            longest.append(s[i])
-            break
-        else:
-            break
+#longLetter = ''
+#
+#for i in range(len(s)):
+#    for x in range(i, len(s)):
+#        if s[i] < s[x]:
+#            longLetter += s[i]
+#            i = x
+#            x += 1
+#
+#
+#
+#
+#print("Longest substring in alphabetical order is: ", longLetter)
 
-print(longest)
+groups = []
+lastChar = ''
+longest = ''
 
+for char in s:
+    if lastChar and c < lastChar:
+        groups.append(longest)
+        longest = char
+    else:
+        longest += char
+    lastChar = char
+print(max(groups, key=len))
