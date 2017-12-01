f85a2ea1a15407ee07d81b9f0cf6910cb7287722
Greg Griffin <griffin.ge@gmail.com>
2017-12-01 09:25:39 -0800

    LF replaced by CRLF

--- week7/pylabex.py
+++ week7/pylabex.py
@@ -14,31 +14,43 @@ for i in range(0, 30):
     myCubic.append(i**3)
     myExponential.append(1.5**i)
 
-#Four kinds of graphs drawn
+# Four kinds of graphs drawn
 
-plt.figure('lin')
-plt.title('Linear')
-plt.xlabel('sample points')
-plt.ylabel('linear function')
-plt.plot(mySamples, myLinear)
+# plt.figure('lin')
+# plt.title('Linear')
+# plt.xlabel('sample points')
+# plt.ylabel('linear function')
+# plt.plot(mySamples, myLinear)
 
-plt.figure('quad')
-plt.title('Quadratic')
-plt.xlabel('sample points')
-plt.ylabel('quadratic function')
-plt.plot(mySamples, myQuadratic)
+# plt.figure('quad')
+# plt.title('Quadratic')
+# plt.xlabel('sample points')
+# plt.ylabel('quadratic function')
+# plt.plot(mySamples, myQuadratic)
 
-plt.figure('cube')
-plt.title('Cubic')
-plt.xlabel('sample points')
-plt.ylabel('cubic function')
-plt.plot(mySamples, myCubic)
+plt.figure('lin quad')
+plt.plot(mySamples, myLinear, label='linear')
+plt.plot(mySamples, myQuadratic, label='quadratic')
+plt.legend()
 
-plt.figure('exp')
-plt.title('Exponential')
-plt.xlabel('sample points')
-plt.ylabel('exponential function')
-plt.plot(mySamples, myExponential)
+# plt.figure('cube')
+# plt.title('Cubic')
+# plt.xlabel('sample points')
+# plt.ylabel('cubic function')
+# plt.yscale('log') ## Can change type of scale
+# plt.plot(mySamples, myCubic)
 
-#Shows the graphs, because drawing turned off by default?
+# plt.figure('exp')
+# plt.title('Exponential')
+# plt.xlabel('sample points')
+# plt.ylabel('exponential function')
+# plt.plot(mySamples, myExponential)
+
+
+plt.figure('cube exp')
+plt.plot(mySamples, myCubic, label='cubic')
+plt.plot(mySamples, myExponential, label='exponential')
+plt.legend()
+
+# Shows the graphs, because drawing turned off by default?
 plt.show()
