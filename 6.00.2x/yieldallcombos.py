f65cdf184e66b421c643f037f7661f68d832d951
Greg Griffin <griffin.ge@gmail.com>
2017-12-30 22:18:12 -0800

    PowerSet yieldAllCombos.py

---
+++ 6.00.2x/yieldallcombos.py
@@ -0,0 +1,43 @@
+# generate all combinations of N items
+
+
+def powerset(items):
+    N = len(items)
+
+    # enumerate the n**N possible combinations
+    for i in range(2**N):
+        combo = []
+        for j in range(N):
+            # test bit jth of integer i
+            if (i >> j) % 2 == 1:
+                combo.append(items[j])
+        yield combo
+
+
+items = ['apple', 'bacon', 'tulip', 'box', 'jumbo', 'micro', 'chair', 'hat']
+#
+# test = powerset(items)
+#
+# for item in test:
+#     print(item)
+#
+#
+
+
+def yieldall(items):
+    N = len(items)
+
+    for i in range(3**N):
+        bag1 = []
+        bag2 = []
+
+        for j in range(N):
+            if (i // 3**j) % 3 == 1:
+                bag1.append(items[j])
+            if (i // 3**j) % 3 == 2:
+                bag2.append(items[j])
+
+        yield (bag1, bag2)
+
+
+test = yieldall(items)
