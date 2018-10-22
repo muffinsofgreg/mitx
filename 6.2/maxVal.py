33396ceb7b67a8754b49f2ff147dc548ccdc6c3b
Greg Griffin <griffin.ge@gmail.com>
2018-10-21 17:49:44 -0700

    add maxVal.py

---
+++ 6.2/maxVal.py
@@ -0,0 +1,21 @@
+def maxVal(toConsider, avail):
+    """Assumes toConsider a list of items, avail a weight.
+    Returns a tuple of the total value of a solution
+    to 0/1 knapsack problem and the items of that
+    solutiion"""
+
+    if toConsider == [] or avail == 0:
+        result = (0, ())
+    elif toConsider[0].getUnits() > avail:
+        result = maxVal(toConsider[1:], avail)
+    else:
+        nextItem = toConsider[0]
+        withVal, withToTake = maxVal(toConsider[1:]), (
+            avail - nextItem.getUnits())
+        withVal += nextItem.getValue()
+        withoutVal, withoutToTake = maxVal(toConsider[1:], avail)
+    if withVal > withoutVal:
+        result = (withVal, withToTake + (nextItem,))
+    else:
+        result = (withoutVal, withoutToTake)
+    return result
