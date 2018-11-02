d29ced2b03964806bc58e660bea4e474bd5ce81f
Greg Griffin <griffin.ge@gmail.com>
2018-11-01 21:59:29 -0700

    black formatting

--- 6.1/week5/hand.py
+++ 6.1/week5/hand.py
@@ -2,25 +2,24 @@ import random
 
 
 class Hand(object):
-
     def __init__(self, n):
-        '''
+        """
         Initialize a Hand.
 
         n: integer, the size of the hand.
-        '''
+        """
         assert type(n) == int
         self.HAND_SIZE = n
-        self.VOWELS = 'aeiou'
-        self.CONSONANTS = 'bcdfghjklmnpqrstvwxyz'
+        self.VOWELS = "aeiou"
+        self.CONSONANTS = "bcdfghjklmnpqrstvwxyz"
 
         # Deal a new hand
         self.dealNewHand()
 
     def dealNewHand(self):
-        '''
+        """
         Deals a new hand, and sets the hand attribute to the new hand.
-        '''
+        """
         # Set self.hand to a new, empty dictionary
         self.hand = {}
 
@@ -36,7 +35,7 @@ class Hand(object):
             self.hand[x] = self.hand.get(x, 0) + 1
 
     def setDummyHand(self, handString):
-        '''
+        """
         Allows you to set a dummy hand. Useful for testing your implementation.
 
         handString: A string of letters you wish to be in the hand. Length of this
@@ -44,26 +43,30 @@ class Hand(object):
 
         This method converts sets the hand attribute to a dictionary
         containing the letters of handString.
-        '''
-        assert len(handString) == self.HAND_SIZE, "Length of handString ({0}) must equal length of HAND_SIZE ({1})".format(len(handString), self.HAND_SIZE)
+        """
+        assert (
+            len(handString) == self.HAND_SIZE
+        ), "Length of handString ({0}) must equal length of HAND_SIZE ({1})".format(
+            len(handString), self.HAND_SIZE
+        )
         self.hand = {}
         for char in handString:
             self.hand[char] = self.hand.get(char, 0) + 1
 
     def calculateLen(self):
-        '''
+        """
         Calculate the length of the hand.
-        '''
+        """
         ans = 0
         for k in self.hand:
             ans += self.hand[k]
         return ans
 
     def __str__(self):
-        '''
+        """
         Display a string representation of the hand.
-        '''
-        output = ''
+        """
+        output = ""
         for letter in sorted(self.hand.keys()):
             output += letter * self.hand[letter]
         return output
